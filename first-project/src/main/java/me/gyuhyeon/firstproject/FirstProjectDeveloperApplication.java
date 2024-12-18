9b9ff2442c52afc7f865bcca5346ab2d1ae44b78
GyuHyeonWkdWkdMan <nero0213@naver.com>
2024-12-18 17:02:14 +0900

    기본 설정, 스웨거 및 의존성 연동

---
+++ first-project/src/main/java/me/gyuhyeon/firstproject/FirstProjectDeveloperApplication.java
@@ -0,0 +1,11 @@
+package me.gyuhyeon.firstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectDeveloperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FirstProjectDeveloperApplication.class, args);
+    }
+}
