c5d26dd05e88d4cb960b324c47494403bc317002
GyuHyeonWkdWkdMan <nero0213@naver.com>
2025-01-14 14:34:45 +0900

    orm-jpa-jdbc

--- first-project/src/main/java/me/gyuhyeon/firstproject/Member.java
+++ first-project/src/main/java/me/gyuhyeon/firstproject/Member.java
@@ -6,15 +6,22 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 @Getter
-@Entity
+@Entity // JPA가 관리하는 엔티티로 지정 - Member 클래스와 실제 데이터베이스의 테이블을 매핑(이름 지정 안하면 클래스 이름과 동일한 테이블과 매핑)
+// 이름 지정 예시
+// @Entity(name = "member_list")
+// public class Article{...}
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자 - protected
+@AllArgsConstructor
 public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", updatable = false)
+    @Id // id 필드를 기본키로 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키를 자동으로 1씩 증가
+    @Column(name = "id", updatable = false) // 변수와 컬럼의 이름이 다르면 @Column(name = "differentid") nullable, unique, columnDefinition 등등 있음
     private Long id;
     @Column(name = "name", nullable = false)
     private String name;
+
+    public void changeName(String name) {
+        this.name = name;
+    }
 }
--- first-project/src/test/java/me/gyuhyeon/firstproject/MemberRepositoryTest.java
+++ first-project/src/test/java/me/gyuhyeon/firstproject/MemberRepositoryTest.java
@@ -14,6 +14,11 @@ class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
 
+    @AfterEach              // 테스트간의 격리를 보장하기 위해 AfterEach 로 deleteAll을 사용
+    public void cleanUp() {
+        memberRepository.deleteAll();
+    }
+
     @Sql("/insert-members.sql")
     @Test
     void getAllMembers() {
@@ -23,6 +28,7 @@ class MemberRepositoryTest {
         // then
         assertThat(members.size()).isEqualTo(3);
     }
+
 
     @Sql("/insert-members.sql")
     @Test
@@ -44,4 +50,62 @@ class MemberRepositoryTest {
         assertThat(member.getId()).isEqualTo(3);
     }
 
+    @Sql("/insert-members.sql")
+    @Test
+    void saveMember(){
+        // given
+        Member member = new Member(1L, "A");
+
+        // when
+        memberRepository.save(member);
+
+        // then
+        assertThat(memberRepository.findById(1L).get().getName()).isEqualTo("A");
+    }
+
+    @Sql("/insert-members.sql")
+    @Test
+    void saveMembers(){
+        // given
+        List<Member> members = List.of(new Member(2L, "B"), new Member(3L, "C"));
+
+        // when
+        memberRepository.saveAll(members);
+
+        // then
+        assertThat(memberRepository.findAll().size()).isEqualTo(3);
+    }
+
+    @Sql("/insert-members.sql")
+    @Test
+    void deleteMemberById() {
+        // when
+        memberRepository.deleteById(2L);
+
+        // then
+        assertThat(memberRepository.findById(2L).isEmpty()).isTrue();
+    }
+
+    @Sql("/insert-members.sql")
+    @Test
+    void deleteAll() {              // 다 지우는 코드라서 잘 안씀
+        // when
+        memberRepository.deleteAll();
+
+        // then
+        assertThat(memberRepository.findAll().size()).isZero();
+    }
+
+    @Sql("/insert-members.sql")
+    @Test
+    void update() {
+        // given
+        Member member = memberRepository.findById(2L).get();
+
+        // when
+        member.changeName("BC");
+
+        // then
+        assertThat(memberRepository.findById(2L).get().getName()).isEqualTo("BC");
+    }
 }
